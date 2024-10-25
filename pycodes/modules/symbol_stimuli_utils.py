f238224611d7ca8ace0ff998fcc0ec6fbc9fe192
ChiaraBoscarino <chiaraboscarino20@gmail.com>
2024-10-25 12:38:47 +0200

    Update symbol_stimuli_utils.py

--- pycodes/modules/symbol_stimuli_utils.py
+++ pycodes/modules/symbol_stimuli_utils.py
@@ -2,8 +2,38 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def symbol_location_check(symbol_center, symbol_size_px, frame_x_size_px, frame_y_size_px):
+    """ Check if the symbol is within the frame boundaries.
+        (0, 0) is the top-left corner of the frame.
+
+        Args:
+            symbol_center: symbol location in pixel coordinates [(x, y) assuming y-axis pointing down and x-axis pointing right]
+            symbol_size_px: symbol dimension (square edge) in pixels
+            frame_x_size_px, frame_y_size_px: frame dimensions in pixels
+    """
+
+    # Get corner coordinates of the symbol (in pixels)
+    symbol_center_x = symbol_center[0]
+    symbol_center_y = symbol_center[1]
+    symbol_top_left_x, symbol_top_left_y = symbol_center_x - symbol_size_px // 2, symbol_center_y - symbol_size_px // 2
+    symbol_top_right_x, symbol_top_right_y = symbol_top_left_x + symbol_size_px, symbol_top_left_y
+    symbol_bottom_left_x, symbol_bottom_left_y = symbol_top_left_x, symbol_top_left_y + symbol_size_px
+    symbol_bottom_right_x, symbol_bottom_right_y = symbol_top_right_x, symbol_bottom_left_y
+
+    # Limits
+    top_limit, bottom_limit, left_limit, right_limit = 0, frame_y_size_px, 0, frame_x_size_px
+
+    # Check if the symbol is within the frame boundaries
+    checks = [top_limit <= y < bottom_limit and left_limit <= x < right_limit for (x,y)
+              in [(symbol_top_left_x, symbol_top_left_y), (symbol_top_right_x, symbol_top_right_y),
+                  (symbol_bottom_left_x, symbol_bottom_left_y), (symbol_bottom_right_x, symbol_bottom_right_y)]]
+    if not all(checks): raise ValueError("Symbol is out of frame boundaries")
+
+    return symbol_center_x, symbol_center_y, symbol_top_left_x, symbol_top_left_y
+
+
 def generate_symbol_frame(symbol,
-                          x_size_px, y_size_px,
+                          frame_x_size_px, frame_y_size_px,
                           symbol_size_px, symbol_center, symbol_color, background_color,
                           show=False):
     """ Generate a frame with a symbol with the specified size and location.
@@ -11,25 +41,22 @@ def generate_symbol_frame(symbol,
 
         Args:
             symbol: any of the following symbols: 'F', 'square', 'T', 'L', 'I'
-            x_size_px, y_size_px: frame dimensions in pixels
+            frame_x_size_px, frame_y_size_px: frame dimensions in pixels
             symbol_size_px: symbol dimension (square edge) in pixels
             symbol_center: symbol location in pixel coordinates [(x, y) assuming y-axis pointing down and x-axis pointing right]
             symbol_color: color value to fill the symbol with
             background_color: color value to fill the frame background with
 
         Returns:
-            frame: numpy array (y_size_px, x_size_px) with the generated frame
+            frame: numpy array (frame_y_size_px, frame_x_size_px) with the generated frame
 
     """
-    frame = np.full((y_size_px, x_size_px), background_color, dtype=np.uint8)
+    frame = np.full((frame_y_size_px, frame_x_size_px), background_color, dtype=np.uint8)
 
-    # Get top-left corner coordinates of the symbol (in pixels)
-    symbol_center_x = symbol_center[0]
-    symbol_center_y = symbol_center[1]
-    symbol_top_left_x = symbol_center_x - symbol_size_px // 2
-    symbol_top_left_y = symbol_center_y - symbol_size_px // 2
-    if symbol_top_left_x < 0 or symbol_top_left_y < 0:
-        raise ValueError("Symbol is out of frame boundaries")
+    # Get coordinates of the symbol (in pixels)
+    (symbol_center_x, symbol_center_y,
+     symbol_top_left_x, symbol_top_left_y) = symbol_location_check(symbol_center, symbol_size_px, frame_x_size_px, frame_y_size_px)
+
     square_dim_row_start = symbol_top_left_y
     square_dim_row_end = symbol_top_left_y + symbol_size_px
     square_dim_col_start = symbol_top_left_x
